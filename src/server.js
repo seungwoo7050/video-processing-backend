0f14406d6aca118bc831dc3b101a9ca0d1baa23a
woopinbell <seungwoo7050@naver.com>
2025-10-04 12:43:24 +0900

    feat: add centralized error-handling middleware

--- src/server.js
+++ src/server.js
@@ -8,6 +8,14 @@ app.use(express.json());
 
 app.use("/api/videos", videoRoutes);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ 
+    error: "서버 오류가 발생했습니다!",
+    message: err.message
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행중입니다`);
 });
